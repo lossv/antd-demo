431f6f599fe5c9e25fc2120a40dbdc15a90e4570
豆七子 <765683668@qq.com>
2021-08-05 22:42:45 +0800

    hrh

---
+++ src/components/login.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="login-container">
+    <h2 class="login-title">智能云联综合管理系统</h2>
+    <a-form ref="form" :model="form" class="login-form">
+      <h2 class="title">用户登录</h2>
+      <a-form-item>
+        <a-input class="inputBox" v-model="form.username">
+          <img :src="user" slot="prefix" alt="">
+        </a-input>
+      </a-form-item>
+      <a-form-item>
+        <a-input-password class="inputBox" v-model="form.password">
+          <img :src="mima" slot="prefix" alt="">
+        </a-input-password>
+      </a-form-item>
+
+      <a-form-item>
+        <a-button class="submit" type="primary" @click="onSubmit">登录</a-button>
+      </a-form-item>
+    </a-form>
+  </div>
+</template>
+
+<script>
+import user from "../assets/user.png"
+import mima from "../assets/密码.png"
+export default {
+  data() {
+    return {
+      user:user,
+      mima:mima,
+      form: {
+        username:"admin",
+        password:"123456"
+      }
+    }
+  },
+  methods: {
+     onSubmit()  {
+      console.log('登陆成功！')
+    },
+  },
+}
+</script>
+<style>
+.login-form {
+  position: relative;
+  width: 1063px;
+  height: 604px;
+  background: url("../assets/pic.png");
+  float: left;
+  left: 350px;
+  top: -200px;
+  background-size: 80%;
+  background-repeat: no-repeat;
+  padding: 40px 110px;
+}
+
+/* 背景 */
+.login-container {
+  position:fixed;
+  top: 0;
+  left: 0;
+  width:100%;
+  height:100%;
+  min-width: 1000px;
+  z-index:-10;
+  zoom: 1;
+  background-color: #fff;
+  background-repeat: no-repeat;
+  background-size: cover;
+  -webkit-background-size: cover;
+  -o-background-size: cover;
+  background-position: center 0;
+  background: url("../assets/bg.png");
+}
+
+/* Log */
+.login-title {
+  letter-spacing:10px;
+  position: relative;
+  top:-100px;
+  color: #fff;
+  text-align: center;
+  margin: 150px 0;
+  font-size: 30px;
+  font-family: Microsoft Yahei;
+}
+/* 登陆按钮 */
+.submit{
+  width: 355px;
+  height:36px;
+  left: 95px;
+  font-size: 17px;
+}
+/* 用户登陆标题 */
+.title{
+  left: 100px;
+  top:30px;
+  position: relative;
+  margin-bottom: 50px;
+  color: #fff;
+  font-weight: 700;
+  font-size: 24px;
+  font-family: Microsoft Yahei;
+}
+/* 输入框 */
+.inputBox{
+  position: relative;
+  height: 45px;
+}
+/* 输入框内左边距50px */
+.ant-input-affix-wrapper .ant-input:not(:first-child) {
+    padding-left: 50px;
+    width: 355px;
+    height: 36px;
+    
+}
+.inputBox{
+  left: 450px;
+}
+</style>
--- src/components/regist.vue
+++ src/components/regist.vue
@@ -1,29 +1,31 @@
 <template>
   <div id="login">
-    <h2 class="fst">智能云联综合管理系统</h2>
+    <h2>智能云联综合管理系统</h2>
     <div class="container">
       <form class="form1" action="#">
         <h2>用户登录</h2>
-        <!-- <div style="overflow: hidden;"> -->
-        <div class="box">
-          <input type="text" placeholder="   UserName" class="input" v-model="UserName"/>
-          <input type="password" placeholder="   PassWord" class="input" v-model="PassWord"/>
-<!--          <input type="text" placeholder="   验证码" class="input" v-model="dtoUserCode" style="width: 40%;float: left;"/>-->
-<!--          <div class="change">换一张</div>-->
-<!--          <div class="s-canvas">-->
-<!--            <canvas-->
-<!--                style="height: 40px;"-->
-<!--                @click="btn2()"-->
-<!--                id="s-canvas"-->
-<!--                :width="contentWidth"-->
-<!--                :height="contentHeight"-->
-<!--            ></canvas>-->
-<!--          </div>-->
+        <div style="overflow: hidden;">
+          <input type="text"  placeholder="UserName" class="input" v-model="UserName" />
+          <input type="password" placeholder="PassWord" class="input" v-model="PassWord" />
+          <input
+            type="text"
+            placeholder="验证码"
+            class="input"
+            v-model="dtoUserCode"
+            style="width: 61%;float: left;"
+          />
+          <div class="s-canvas">
+            <canvas
+              style="height: 40px;"
+              @click="btn2()"
+              id="s-canvas"
+              :width="contentWidth"
+              :height="contentHeight"
+            ></canvas>
+          </div>
         </div>
-        <button class="btn" @click="btn()">登录</button>
-        <a href="#" class="t1">忘记密码？</a>
-        <a href="#" class="t2">还没有账号?</a>
-        <a href="#" class="t3">去注册</a>
+        <!-- <span>欢迎来到后台管理系统</span> -->
+        <button v-loading.fullscreen.lock="fullscreenLoading" class="btn" @click="btn()">登录</button>
       </form>
     </div>
   </div>
@@ -31,9 +33,8 @@
 
 <script>
 import axios from "axios";
-
 export default {
-  name: "login", /***** */
+  name:"login",/***** */
   props: {
     identifyCode: {
       type: String,
@@ -88,7 +89,7 @@ export default {
       default: 31
     }
   },
-  created: function () {
+  created: function() {
     this.btn2();
   },
   data() {
@@ -102,17 +103,18 @@ export default {
   methods: {
     btn2() {
       var identifyCode = "";
-      //看需求设置长度，我这里设置了4
+      //设置长度，这里看需求，我这里设置了4
       var codeLength = 4;
 
       //设置随机字符
-      var random = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+      var random = new Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
 
-      //设置的4则循环4次
+      //循环codeLength 我设置的4就是循环4次
       for (var i = 0; i < codeLength; i++) {
-        //设置随机数范围,这设置为0 ~36
+        //设置随机数范围,这设置为0 ~ 36
         var index = Math.floor(Math.random() * 9);
-        //将每次随机的字符进行拼接
+
+        //字符串拼接 将每次随机的字符 进行拼接
         identifyCode += random[index];
       }
       //将拼接好的字符串赋值给展示的code
@@ -129,27 +131,27 @@ export default {
       let b = this.randomNum(min, max);
       return "rgb(" + r + "," + g + "," + b + ")";
     },
-    // drawPic() {
-    //   let canvas = document.getElementById("s-canvas");
-    //   let ctx = canvas.getContext("2d");
-    //   ctx.textBaseline = "bottom";
-    //   // 绘制背景
-    //   ctx.fillStyle = this.randomColor(
-    //       this.backgroundColorMin,
-    //       this.backgroundColorMax
-    //   );
-    //   ctx.fillRect(0, 0, this.contentWidth, this.contentHeight);
-    //   // 绘制文字
-    //   for (let i = 0; i < this.identifyCode.length; i++) {
-    //     this.drawText(ctx, this.identifyCode[i], i);
-    //   }
-    //   this.drawLine(ctx);
-    //   this.drawDot(ctx);
-    // },
+    drawPic() {
+      let canvas = document.getElementById("s-canvas");
+      let ctx = canvas.getContext("2d");
+      ctx.textBaseline = "bottom";
+      // 绘制背景
+      ctx.fillStyle = this.randomColor(
+        this.backgroundColorMin,
+        this.backgroundColorMax
+      );
+      ctx.fillRect(0, 0, this.contentWidth, this.contentHeight);
+      // 绘制文字
+      for (let i = 0; i < this.identifyCode.length; i++) {
+        this.drawText(ctx, this.identifyCode[i], i);
+      }
+      this.drawLine(ctx);
+      this.drawDot(ctx);
+    },
     drawText(ctx, txt, i) {
       ctx.fillStyle = this.randomColor(this.colorMin, this.colorMax);
       ctx.font =
-          this.randomNum(this.fontSizeMin, this.fontSizeMax) + "px SimHei";
+        this.randomNum(this.fontSizeMin, this.fontSizeMax) + "px SimHei";
       let x = (i + 1) * (this.contentWidth / (this.identifyCode.length + 1));
       let y = this.randomNum(this.fontSizeMax, this.contentHeight - 5);
       var deg = this.randomNum(-45, 45);
@@ -165,17 +167,17 @@ export default {
       // 绘制干扰线
       for (let i = 0; i < 5; i++) {
         ctx.strokeStyle = this.randomColor(
-            this.lineColorMin,
-            this.lineColorMax
+          this.lineColorMin,
+          this.lineColorMax
         );
         ctx.beginPath();
         ctx.moveTo(
-            this.randomNum(0, this.contentWidth),
-            this.randomNum(0, this.contentHeight)
+          this.randomNum(0, this.contentWidth),
+          this.randomNum(0, this.contentHeight)
         );
         ctx.lineTo(
-            this.randomNum(0, this.contentWidth),
-            this.randomNum(0, this.contentHeight)
+          this.randomNum(0, this.contentWidth),
+          this.randomNum(0, this.contentHeight)
         );
         ctx.stroke();
       }
@@ -186,124 +188,120 @@ export default {
         ctx.fillStyle = this.randomColor(0, 255);
         ctx.beginPath();
         ctx.arc(
-            this.randomNum(0, this.contentWidth),
-            this.randomNum(0, this.contentHeight),
-            1,
-            0,
-            2 * Math.PI
+          this.randomNum(0, this.contentWidth),
+          this.randomNum(0, this.contentHeight),
+          1,
+          0,
+          2 * Math.PI
         );
         ctx.fill();
       }
     },
     btn() {
       var that = this;
-      if (this.UserName === "") {
+      // let param = {
+      //   dtoUserName: this.UserName,
+      //   password: this.PassWord
+      // };
+      if (this.UserName == "") {
         this.$message.error("请输入正确的账号");
         return false;
-      } else if (this.PassWord === "") {
+      } else if (this.PassWord == "") {
         this.$message.error("请输入正确的密码");
         return false;
-      } else{
-
-        //this.fullscreenLoading = true;
-
+      } else if (this.dtoUserCode == "") {
+        this.$message.error("请输入验证码");
+      } else if (this.dtoUserCode !== this.identifyCode) {
+        this.$message.error("验证码错误");
+      } else if (this.dtoUserCode == this.identifyCode) {
+        this.$message({
+          message: "登陆成功",
+          type: "success"
+        });
+        this.fullscreenLoading = true;
+        
         axios
-            .post("https://www.dlxxcd.com/aidSystem/user/userLogin", {
-              params: {
-                phone: that.UserName,
-                password: that.PassWord
-              }
-            })
-            .then(res => {
-              setTimeout(() => {
-              }, 3000);
-              console.log(res);
-            })
+          .get(window.g.baseURL + "workOrder/login", {
+            params: {
+              userName: that.UserName, // E00000
+              passWord: that.PassWord // 123456
+            }
+          })
+          .then(res => {
+            setTimeout(() => {
+              this.fullscreenLoading = false;
+            }, 3000);
+            this.$router.push({ path: "/index" });
+            console.log(res);
+          })
+          .catch();
       }
     }
   },
   watch: {
-    // identifyCode() {
-    //   this.drawPic();
-    // }
+    identifyCode() {
+      this.drawPic();
+    }
   },
-  // mounted() {
-  //   this.drawPic();
-  // }
+  mounted() {
+    this.drawPic();
+  }
 };
 </script>
 
 <style scoped>
 #login {
-  position: relative;
   align-items: center;
   background: url("../assets/bg.png");
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
   display: grid;
+  height: 100vh;
   place-items: center;
 }
-
 .container {
-  position: relative;
-  max-width: 900px;
-  margin: 10px 18px;
+  max-width: 400px;
+  background: #e9e9e9;
+  background: rgba(99, 99, 100, 0.4);
+  border-radius: 15px;
+  box-shadow: 0 0.9rem 1.7rem rgba(0, 0, 0, 0.25),
+    0 0.7rem 0.7rem rgba(0, 0, 0, 0.22);
+  margin: 0 18px;
 }
-
 .form1 {
-  border-style: dashed;
-  color: rgba(255, 255, 255, 0.699);
-  position: relative;
   background: url("../assets/pic.png");
-  padding: 100px 190px 160px 190px;
-  margin-top: 150px;
+  padding: 2.75rem;
   text-align: center;
 }
-
-.fst {
-  position: relative;
-  top: 30px;
+h2 {
   font-weight: 400;
   margin-bottom: 1.25rem;
   text-align: center;
   color: #fff;
-  letter-spacing: 10px;
-}
-
-.form1 h2 {
-  position: relative;
-  font-weight: 400;
-  margin-bottom: 1.25rem;
-  text-align: center;
-  color: #fff;
-  left: 250px;
 }
 
 .input {
-  display: inline-block;
-  float: left;
-  left: 320px;;
-  position: relative;
   background-color: #fff;
   border: none;
   padding: 0.9rem 0.9rem;
-  margin: 0.5rem 0.5rem;
-  width: 68%;
+  margin: 0.5rem 0;
+  width: 100%;
+  /* border-radius: 6px; */
   box-sizing: border-box;
   -webkit-box-sizing: border-box;
 }
-
 span {
   color: #8b8b8b;
   text-align: center;
   display: block;
-  margin-top: 250px;
+  margin-top: 25px;
 }
-
 .btn {
-  position: relative;
-  left: 325px;
-  width: 68%;
   background-image: linear-gradient(90deg, #0367a6 0%, #008997 74%);
-  /*border: 1px solid var(--blue);*/
+  /* border-radius: 20px; */
+  border: 1px solid var(--blue);
   cursor: pointer;
   font-size: 0.8rem;
   font-weight: bold;
@@ -315,7 +313,9 @@ span {
   margin-top: 20px;
   transition: 0.8s !important;
 }
-
+.btn:hover {
+  border-radius: 50px;
+}
 .s-canvas {
   padding: 4px 0;
   height: 38px;
@@ -323,62 +323,13 @@ span {
   padding: 3px 0;
   float: right;
 }
-
 #s-canvas {
+  /* border-radius: 10px; */
   margin-top: 0;
   cursor: pointer;
 }
-
 .s-canvas canvas {
-  position: relative;
   margin-top: 1px;
   margin-left: 8px;
-  float: left;
-  left: 130px;
-}
-
-.box {
-  position: relative;
-  top: 10px;
-  margin: 1px;
-}
-
-.container .form1 {
-  position: relative;
-  top: -100px;
-}
-
-.change {
-  position: relative;
-  top: 145px;
-  left: 226px;
-  font-size: 1px;
-  color: rgb(83, 228, 238);
-}
-
-a {
-  text-decoration: none;
-  position: relative;
-  font-size: 12px;
-  color: #fff;
-}
-
-.t1 {
-  position: relative;
-  left: -20px;
-  top: 40px;
-}
-
-.t2 {
-  position: relative;
-  left: 120px;
-  top: 40px;
-}
-
-.t3 {
-  position: relative;
-  left: 130px;
-  top: 40px;
-  color: rgb(255, 230, 0);
 }
 </style>
